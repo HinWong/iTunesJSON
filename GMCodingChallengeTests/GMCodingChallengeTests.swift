177f7cb789b8f9f8a0c5d5627ca5bea267c11cb8
Hin Wong <hinwong@Hins-MacBook-Pro.local>
2021-06-07 21:16:51 -0400

    Added one unit test

---
+++ GMCodingChallengeTests/GMCodingChallengeTests.swift
@@ -0,0 +1,21 @@
+//
+//  GMCodingChallengeTests.swift
+//  GMCodingChallengeTests
+//
+//  Created by Hin Wong on 6/7/21.
+//
+
+import XCTest
+@testable import GMCodingChallenge
+
+class GMCodingChallengeTests: XCTestCase {
+    
+    var sut: MusicResponse?
+    
+    func test_Decode() {
+        guard let sut = sut else {return}
+        let musicArray = sut.results
+        XCTAssertEqual(musicArray.count, 1, "Should return at least 1 result")
+    }
+
+}
---
+++ GMCodingChallengeTests/NetworkRequestTests.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkRequestTests.swift
+//  GMCodingChallengeTests
+//
+//  Created by Hin Wong on 6/7/21.
+//
+
+import XCTest
+@testable import GMCodingChallenge
+
+class NetworkRequestTests: XCTestCase {
+    
+    var sut: NetworkManager?
+    
+}
